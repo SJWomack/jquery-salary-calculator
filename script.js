05a39df8cd40c959d4c6ecbea097ed51395aff00
Stephen Womack <swomack111@gmail.com>
2022-05-14 15:01:00 -0500

    delete function

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(readyNow);
 
 function readyNow() {
-   
+
     $(document).on('click', '#add-button', appendDom);
+    $(document).on('click', '#delete-btn', deleteButton);
 }
 let newEmployeeArray = []
-function newEmployee(firstName,lastName,idNumber, jobTitle, annualSalary){
+function newEmployee(firstName, lastName, idNumber, jobTitle, annualSalary) {
     const newEmployeeObject = {
         first: firstName,
         last: lastName,
@@ -17,39 +18,48 @@ function newEmployee(firstName,lastName,idNumber, jobTitle, annualSalary){
     newEmployeeArray.push(newEmployeeObject);
 }
 
-function appendDom(){
+function appendDom() {
     firstName = $('#emp-first-name').val()
     lastName = $('#emp-last-name').val()
     idNumber = $('#id').val()
     jobTitle = $('#job-title').val()
     annualSalary = $('#annual-sal').val()
 
-    newEmployee(firstName,lastName,idNumber, jobTitle, annualSalary);
+    newEmployee(firstName, lastName, idNumber, jobTitle, annualSalary);
     console.log(newEmployeeArray)
-    
-    if (!firstName || !lastName|| !idNumber || !jobTitle|| !annualSalary){     // tried to utilize if inputField == null but it doesnt work?
+
+    if (!firstName || !lastName || !idNumber || !jobTitle || !annualSalary) {     // tried to utilize if inputField == null but it doesnt work?
         alert('Please fill in all input fields.')
     }
 
     $('.employee-input').val('')
 
-   for (let employees of newEmployeeArray){
-    $('#employee-calc-table').append(`
+    for (let employees of newEmployeeArray) {
+        $('#calc-table-body').append(`
     <tr>
         <td>${employees.first}</td>
         <td>${employees.last}</td>
         <td>${employees.id}</td>
         <td>${employees.title}</td>
-        <td>${employees.annualSalary}</td>
+        <td>${employees.salary}</td>
         
         <td>
-                </button>
-            <button class="delete-btn">
+                
+            <button id="delete-btn">
                 Delete Line
             </button>
         </td>
     </tr>  
 `);
-   }
+    }
+}
+
+function deleteButton(){
+    tr = $(this).parent().parent();
+
+    let empName = tr.first().val();
+    alert(`Removed ${empName}'s data.`)
+
+    tr.remove()
 }
 
